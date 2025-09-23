7cf05bb9b4d9788e06f6e857457e0d4cfdef0fa5
lpascalis <lpascalis@gmail.com>
2025-09-23 23:04:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nuragica-v5';
+const CACHE_NAME = 'nuragica-v6';
 const ASSETS = [
                   './assets/icons/splash-dark-1170x2532.png',
 './assets/icons/splash-light-1170x2532.png',
